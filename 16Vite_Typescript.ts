c6d8b46650c0acd527f555d18d0c075804f11b7f
DeborahRise <debie.rise.bookings@gmail.com>
2025-01-14 23:47:58 +0100

    Add 16Vite_Typescript.ts: Created Vite configuration file for TypeScript project. Configured build output directory and development server settings.

---
+++ 16Vite_Typescript.ts
@@ -0,0 +1,4 @@
+//npm create vite@latest - the command to run to initialize a ts project with vite
+// vite.config.ts is where we add our pluggins
+// Vite documentation: Go to "DEPLOYING A STATIC SITE" Learn how to deploy or build your project using vercel, netlify, etc. highly simplified
+// Also not the difference in the Env Variables and Modes - as against Process.env, we have "import.meta.env.MODE: string"
