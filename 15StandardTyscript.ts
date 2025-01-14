ede1f9de74ae58d9e2531d77b66d83ba6564882f
DeborahRise <debie.rise.bookings@gmail.com>
2025-01-14 01:13:39 +0100

    Added 15StandardTyscript.ts: Provided standard TypeScript practices, including type annotations, interfaces, and type assertions.

---
+++ 15StandardTyscript.ts
@@ -0,0 +1,5 @@
+// Create a file called "types.ts" - this will contain all the different types definition for a project. so define all your interfaces and class and so on
+
+// And the code gets bigger, we may need to break into multiple files, based on where they are gonna be used. eg. in src/components/types.ts for all types defined in the the components.
+
+// Note that src can also have its own types.ts file that has shares types for all directories under it
