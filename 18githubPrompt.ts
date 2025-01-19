f1037671f59227d2a7ff850891715afee14a72de
DeborahRise <debie.rise.bookings@gmail.com>
2025-01-19 23:54:22 +0100

    Add 18githubPrompt.ts: Introduced a script for generating engaging GitHub prompts, focusing on highlighting user experience, skill sets, and project showcases with dynamic and customizable templates.

---
+++ 18githubPrompt.ts
@@ -0,0 +1,12 @@
+// With all the details you have on this project, please git add and very very detailed commit messages, for each file, on seperate line, make it a block of codes, no comments needed. make commits messages as detailed as possible
+// use this format:
+// git add src/pages/singlepage/singlepageSpare.scss
+// git commit -m "Add singlepageSpare.scss: Created new SCSS file for SingleSparepartPage component with detailed styling for various elements."
+
+
+// modified:   ../16Vite_Typescript.ts
+// modified:   ../17React+Typescript.ts
+
+// Untracked files:
+// (use "git add <file>..." to include in what will be committed)
+// ./
